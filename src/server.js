80ebc5b639685dc9d598d908b3a1d3ab111e373c
Kris Dowe <57381168+kristoferdowe@users.noreply.github.com>
2022-04-03 18:05:06 -0400

    added basic server logic

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
+// console.log(process.env);
 const request = require('request');
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const getToken = (url, callback) => {
 
-    const options = {
+const options = {
         url: process.env.GET_TOKEN,
         uri:'https://id.twitch.tv/oauth2/token',
         json:true,
@@ -19,20 +20,41 @@ const getToken = (url, callback) => {
         if (err) {
             return console.log(err);
         }
-        console.log('status: ${res.statusCode}');
-        console.log(body);
-
+        console.log(res.body);
         callback(res);
     });
 };
 
-var AT = '';
+ var AT = '';
 
 getToken(process.env.GET_TOKEN, (res) => {
    AT = res.body.access_token;
    return AT;
 })
 
-setTimeout (() => {
-    console.log(AT); 
-}, 1000)
+// const getGames = (url, accessToken, callback) => {
+// const gameOptions = {
+//     url: process.env.GET_GAMES,
+//     method:'GET',
+//     headers: {
+//         'Client-Id':process.env.CLIENT_ID,
+//         'Authorization': 'Bearer ' + accessToken
+//     }
+// };
+// request.get(gameOptions, (err,res,body) => {
+//     if (err) {
+//         return console.log(err);
+
+//     }
+//     console.log('Status : ${res.statusCode}');
+//     console.log(JSON.parse(body));
+//     })
+ 
+// };
+
+
+// setTimeout (() => {
+// getGames(process.env.GET_GAMES, AT,(response)=> {
+    
+//     })
+// },2000) 
