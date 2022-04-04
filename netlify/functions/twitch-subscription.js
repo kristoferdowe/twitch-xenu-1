1a0adb8fa459b5ccb9ad5fed152c3f2e6cadeed7
Kris Dowe <57381168+kristoferdowe@users.noreply.github.com>
2022-04-04 01:32:50 -0400

    added show subscriptions

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -1,14 +1,26 @@
 exports.handler = async (event) => {
-
+    
     const { headers } = event;
 
     const type = headers['twitch-eventsub-message-type'] || 'no type';
+    const eventType = headers['twitch-eventsub-subscription-type'];
+
+
+    if(type !== 'notification' || eventType!== 'channel.subscribe') {
+        return { statusCode: 200, body: ''};
+    }
+
+    console.log({headers: event.headers, body: event.body});
+
+    //at this point you know you have a valid subscription
+    //add cool stuff here
+    const { event: twitchEvent } = JSON.parse(event.body);
+    const user = twitchEvent.user_name;
+
+    console.log(`${user} just subscribed!`);
 
     return{
         statusCode:200,
-        body:JSON.stringify({
-            type,
-            message:'ok',
-        })
-    };
-}
+        body:'',
+};
+};                 
