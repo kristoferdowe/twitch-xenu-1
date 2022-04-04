9dff478e181128b809925a49205ff8ee4b0933f1
Kris Dowe <57381168+kristoferdowe@users.noreply.github.com>
2022-04-03 21:33:52 -0400

    testing simple server for netlify

--- src/server.js
+++ src/server.js
@@ -1,36 +1,52 @@
-require('dotenv').config();
-// console.log(process.env);
-const request = require('request');
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
-const getToken = (url, callback) => {
+// require('dotenv').config();
+// // console.log(process.env);
+// const request = require('request');
+// const client_id = process.env.CLIENT_ID;
+// const client_secret = process.env.CLIENT_SECRET;
+// const getToken = (url, callback) => {
 
-const options = {
-        url: process.env.GET_TOKEN,
-        uri:'https://id.twitch.tv/oauth2/token',
-        json:true,
-        body: {
-            client_id: client_id,
-            client_secret: client_secret,
-            grant_type: 'client_credentials',
-        }
+// const options = {
+//         url: process.env.GET_TOKEN,
+//         uri:'https://id.twitch.tv/oauth2/token',
+//         json:true,
+//         body: {
+//             client_id: client_id,
+//             client_secret: client_secret,
+//             grant_type: 'client_credentials',
+//         }
+//     };
+
+//     request.post(options, (err, res, body) => {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log(res.body);
+//         callback(res);
+//     });
+// };
+
+//  var AT = '';
+
+// getToken(process.env.GET_TOKEN, (res) => {
+//    AT = res.body.access_token;
+//    return AT;
+// })
+
+exports.handler = async (event) => {
+
+    const { headers } = event;
+
+    const type = headers['twitch-eventsub-message-type'] || 'no type';
+
+    return{
+        statusCode:200,
+        body:JSON.stringify({
+            type,
+            message:'ok',
+        })
     };
+}
 
-    request.post(options, (err, res, body) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(res.body);
-        callback(res);
-    });
-};
-
- var AT = '';
-
-getToken(process.env.GET_TOKEN, (res) => {
-   AT = res.body.access_token;
-   return AT;
-})
 
 // const getGames = (url, accessToken, callback) => {
 // const gameOptions = {
