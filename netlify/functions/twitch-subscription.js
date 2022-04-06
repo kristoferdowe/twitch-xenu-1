7916aedaf9bb347b5e9b5dd77c9c8465161f931a
Kris Dowe <57381168+kristoferdowe@users.noreply.github.com>
2022-04-06 19:46:44 -0400

    modified body callback

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -21,6 +21,16 @@ exports.handler = async (event) => {
 
     return{
         statusCode:200,
-        body:'',
+        body:{
+            "type": "channel.follow",
+            "version": "1",
+            "condition": {
+            "broadcaster_user_id": "236604792"
+            },
+            "transport": {
+              "method": "webhook",
+              "callback": "https//:twitch-eventsub-app.netlify.app/.netlify/functions/twitch-subscription",
+              "secret": "random"
+            }}
 };
 };                 
